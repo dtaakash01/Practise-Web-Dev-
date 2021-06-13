2833b5a9db715b309ee6e9ae40d913ac6b5c25a0
Akash Dixit <akashdixit@localhost.localdomain>
2021-06-13 20:30:42 +0530

    views learnt

--- node/meadowlark/meadowlark.js
+++ node/meadowlark/meadowlark.js
@@ -5,21 +5,36 @@ var app = express();
 app.set('port',3000 || process.env.PORT);
 
 
-//Setting 404 page
-app.use(function(req,res) {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 NOT FOUND');
-})
 
-// custom 500 page
-app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 - Server Error');
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+                .create({ defaultLayout:'main' });
+
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+app.get('/', function(req, res) {
+    res.render('home');
     });
 
+app.get('/about', function(req, res) {
+    res.render('about');
+    });
+
+    // 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+    });
+    
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+    });
 
 app.listen(app.get('port'), function(){
     console.log( 'Express started on http://localhost:' +
